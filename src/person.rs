d72017d92723be95e7dd99e0463902d40900bc7a
layonthehorn <thomaspdressel@gmail.com>
2020-09-12 16:54:47 -0700

    added new enums and structs

--- src/person.rs
+++ src/person.rs
@@ -1,21 +1,83 @@
-pub struct Person {
+#[derive(Clone,Copy)]
+pub enum Gender{
+    Male,
+    Female
+}
+
+#[derive(Clone,Copy)]
+pub enum Nationality {
+    UnitedStates,
+    Australia,
+    UnitedKingdom,
+    Germany
+}
+
+struct Skills{
+    can_drive: bool,
+    can_swim: bool
+
+}
+
+impl Skills{
+    fn new() -> Skills{
+        Skills{
+            can_drive: false,
+            can_swim: false
+        }
+    }
+    fn set_can_swim(&mut self, value: bool){
+        // prevents setting the value to false
+        if value{
+            self.can_swim = value;
+        }
+    }
+
+    fn set_can_drive(&mut self, value: bool){
+        // prevents setting the value to false
+        if value{
+            self.can_drive = value;
+        }
+    }
+}
+
+struct Name{
+
     first_name: String,
     last_name: String,
     middle_name: String,
+}
+
+impl Name {
+    fn new(f: String, m:String,l:String)-> Name{
+        Name{
+            first_name:f,
+            middle_name: m,
+            last_name: l,
+        }
+    }
+}
+
+pub struct Person {
+    names: Name,
     age: u32,
+    gender: Gender,
+    nationality: Nationality,
     friends: Vec<Person>,
     family: Vec<Person>,
+    skills: Skills,
 }
 
 impl Person {
-    pub fn new(first: String, middle: String, last: String, a: u32) -> Person {
+    pub fn new(first: String, middle: String, last: String, a: u32, gend: Gender, nation: Nationality) -> Person {
         Person{
-            first_name: first,
-            last_name: last,
-            middle_name: middle,
+            names: Name::new(first,middle,last),
             age: a,
+            gender: gend,
+            nationality: nation,
             friends: vec![],
-            family: vec![]
+            family: vec![],
+            skills: Skills::new()
+
         }
 
     }
@@ -31,7 +93,15 @@ impl Person {
     }
 
     pub fn get_full_name(&self) -> String {
-        format!("{} {} {}",self.first_name, self.middle_name, self.last_name)
+        format!("{} {} {}",self.names.first_name, self.names.middle_name, self.names.last_name)
+    }
+
+    pub fn get_gender(&self) -> Gender {
+        self.gender
+    }
+
+    pub fn get_nationality(&self) -> Nationality {
+        self.nationality
     }
 
 }
